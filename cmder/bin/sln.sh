dbad6c11082370c1055f1f2724e3d6131ddf3858
Ryan Morrin <ryanmorrin@gmail.com>
2015-04-01 10:57:23 +0100

    Add support for VS2015 to `sln`

--- cmder/bin/sln.sh
+++ cmder/bin/sln.sh
@@ -6,6 +6,8 @@ sln=$(ls -a | grep -E '\.sln')
 if [ -n "$sln" ]; then
 	if [ "$1" == "--vs12" ]; then
 		"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" "${sln}" &
+	elif [ "$1" == "--vs15" ]; then
+		"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "${sln}" &
 	else
 		"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "${sln}" &
 	fi
