6d4e247bb4ceca3b3914035d84326dba7388497d
Ryan Morrin <ryanmorrin@gmail.com>
2017-08-08 17:13:51 +0100

    Add bash script to restore VS Code extensions
    Credit to: http://www.growingwiththeweb.com/2016/06/syncing-vscode-extensions.html

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Credit: http://www.growingwiththeweb.com/2016/06/syncing-vscode-extensions.html
+
+EXTENSIONS=(
+  "arcticicestudio.nord-visual-studio-code" \
+  "dbaeumer.vscode-eslint" \
+  "jchannon.csharpextensions" \
+  "ms-vscode.csharp" \
+  "PeterJausovec.vscode-docker" \
+  "qinjia.seti-icons" \
+  "zhuangtongfa.Material-theme"
+)
+
+for VARIANT in "code" \
+               "code-insiders"
+do
+  if hash $VARIANT 2>/dev/null; then
+    echo "Installing extensions for $VARIANT"
+    for EXTENSION in ${EXTENSIONS[@]}
+    do
+      $VARIANT --install-extension $EXTENSION
+    done
+  fi
+done
