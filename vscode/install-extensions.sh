906863d875fcca8e9c32ecb6e8766441a014de2f
Ryan Morrin <ryanmorrin@gmail.com>
2019-11-15 15:02:52 +0000

    Add windows terminal config, update vscode settings/extensions

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -3,6 +3,7 @@
 # Credit: http://www.growingwiththeweb.com/2016/06/syncing-vscode-extensions.html
 
 EXTENSIONS=(
+  "amatiasq.sort-imports" \
   "Angular.ng-template" \
   "arcticicestudio.nord-visual-studio-code" \
   "christian-kohler.path-intellisense" \
@@ -16,13 +17,15 @@ EXTENSIONS=(
   "johnpapa.angular-essentials" \
   "johnpapa.Angular2" \
   "johnpapa.vscode-peacock" \
-  "ms-mssql.mssql" \
   "ms-vscode.csharp" \
   "ms-vscode.vscode-typescript-tslint-plugin" \
   "msjsdiag.debugger-for-chrome" \
   "natewallace.angular2-inline" \
   "nrwl.angular-console" \
+  "PKief.material-icon-theme" \
   "qinjia.seti-icons" \
+  "sdras.night-owl" \
+  "stringham.move-ts" \
 )
 
 for VARIANT in "code" \
